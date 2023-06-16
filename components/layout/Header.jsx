086b716498359a1fd391c09f3286c8967f91530b
hilalozyurtt <hilalozyurt1256@hotmail.com>
2023-06-16 22:15:00 +0300

    Header Component ve front-end başlangıcı

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
+import {useState} from 'react'
 import Logo from '../ui/Logo'
 import {FaUserAlt, FaShoppingCart, FaSearch} from "react-icons/fa"
+import {GiHamburgerMenu} from "react-icons/gi"
+import {AiFillCloseCircle} from "react-icons/ai"
+import Search from '../ui/Search'
 
 const Header = () => {
+  const [isSearchModal, setIsSearchModal] = useState(false)
+  const [isMenuModal, setIsMenuModal] = useState(false)
+
   return (
     <div className='h-[5.5rem] bg-secondary'>
       <div className='container mx-auto text-white flex justify-between items-center h-full'>
         <Logo/>
-        <nav>
-          <ul className='flex gap-x-2'>
+        <nav className={`sm:static absolute top-0 left-0 grid place-content-center 
+         sm:w-auto sm:h-auto w-full h-full sm:text-white text-black bg-white sm:bg-transparent
+         ${isMenuModal !== true && "sm:flex hidden"}`}>
+          <ul className='flex gap-x-2 sm:flex-row flex-col items-center'>
             <li className='px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer'><a href=''>Home</a></li>
             <li className='px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer'><a href=''>Menu</a></li>
             <li className='px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer'><a href=''>About</a></li>
             <li className='px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer' ><a href=''>Book Table</a></li>
           </ul>
+          {isMenuModal && ( 
+          <button className="absolute top-4 right-4 " onClick={() => setIsMenuModal(false)} >
+              <AiFillCloseCircle size={25} className="text-primary transition-all"/>
+          </button>)}
         </nav>
         <div className='flex gap-x-4 items-center'>
-            <a href='#'><FaUserAlt/></a>
-            <a href='#'><FaShoppingCart/></a>
-            <a href='#'><FaSearch/></a>
-            <a href='#'><button className='btn-primary'> Order Online </button></a> 
+            <a href='#'><FaUserAlt className='hover:text-primary transition-all'/></a>
+            <a href='#'><FaShoppingCart className='hover:text-primary transition-all'/></a>
+            <button href='#' onClick={() => setIsSearchModal(true)}><FaSearch className='hover:text-primary transition-all'/></button>
+            <a href='#' className="md:inline-block hidden"><button className='btn-primary'> Order Online </button></a> 
+            <button className='sm:hidden inline-block' onClick={() => setIsMenuModal(true)}><GiHamburgerMenu className='hover:text-primary transition-all text-xl'/></button>
         </div>
       </div>
+      {isSearchModal && <Search setIsSearchModal={setIsSearchModal}/>}
     </div>
   )
 }
